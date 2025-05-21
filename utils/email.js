f2e8f4ffa1caf885b83ee9f24203e6c20ede874d
Ikechukwu Nwuba <ikechukwujo45@gmail.com>
2025-05-21 10:40:16 +0100

    gmail smtp issue

--- utils/email.js
+++ utils/email.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, // Use TLS (STARTTLS)
+  port: 465,
+  // secure: false, // Use TLS (STARTTLS)
   auth: {
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD,
   },
-  tls: {
-    rejectUnauthorized: false, // Accept self-signed certs
-  },
+  // tls: {
+  //   rejectUnauthorized: false, // Accept self-signed certs
+  // },
 });
 
 module.exports = transporter;
